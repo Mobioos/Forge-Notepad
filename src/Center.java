541b4bd52176289f22fca00f047eae731071e2fd
Karim Ghallab <Karim.ghallab@mobioos.ai>
2023-05-09 12:57:45 +0200

    First commit

---
+++ src/Center.java
@@ -0,0 +1,35 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+/**
+ * A PUBLIC CLASS FOR CENTER.JAVA
+ */
+public class Center {
+
+    Notepad n;
+
+    // for using the object in the Notepad.java
+    Fonts f;
+
+    // for using the object in the Fonts.java
+    public Center(Notepad n) {
+        this.n = n;
+    }
+
+    public Center(Fonts f) {
+        this.f = f;
+    }
+
+    public void nCenter() {
+        // Centering the window
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        n.setLocation((screenSize.width - n.getWidth()) / 2, (screenSize.height - n.getHeight()) / 2);
+    }
+
+    public void fCenter() {
+        // Centering the window
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        f.setLocation((screenSize.width - f.getWidth()) / 2, (screenSize.height - f.getHeight()) / 2);
+    }
+
+}
---
+++ src/UndoAction.java
@@ -0,0 +1,43 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
+import javax.swing.undo.CannotUndoException;
+
+class UndoAction extends AbstractAction {
+
+    private static final long serialVersionUID = 1;
+
+    Notepad notepad;
+
+    public UndoAction(Notepad notepad) {
+        super("Undo");
+        putValue(Action.SMALL_ICON,
+                new ImageIcon(this.getClass().getResource("images/undo.gif")));
+        setEnabled(false);
+        this.notepad = notepad;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        try {
+            notepad.undo.undo();
+        } catch (CannotUndoException ex) {
+            System.out.println("Unable to undo: " + ex);
+            ex.printStackTrace();
+        }
+        update();
+        notepad.redoAction.update();
+    }
+
+    protected void update() {
+        if (notepad.undo.canUndo()) {
+            setEnabled(true);
+            putValue("Undo", notepad.undo.getUndoPresentationName());
+        } else {
+            setEnabled(false);
+            putValue(Action.NAME, "Undo");
+        }
+    }
+
+}
